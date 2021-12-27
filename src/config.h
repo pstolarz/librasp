8a663fc8f485178c28fed41e1746b3f979aff673
Piotr Stolarz <pstolarz@o2.pl>
2021-12-27 16:04:06 +0100

    Configuration parameters may be set separately

--- src/config.h
+++ src/config.h
@@ -1,19 +1,39 @@
+/*
+   Copyright (c) 2015,2021 Piotr Stolarz
+   librasp: RPi HW interface library
+
+   Distributed under the 2-clause BSD License (the License)
+   see accompanying file LICENSE for details.
+
+   This software is distributed WITHOUT ANY WARRANTY; without even the
+   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   See the License for more information.
+ */
+
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
 /* Add support for system clock driver. Usage of this driver is problematic
    due to its inaccuracy. */
-//#define CONFIG_CLOCK_SYS_DRIVER
+#ifndef CONFIG_CLOCK_SYS_DRIVER
+//# define CONFIG_CLOCK_SYS_DRIVER
+#endif
 
 /* BCM's GPIO events support. Usage of these events is intended via kernel
    executives (interrupt specific functionality). BCM's events usage in the
    userland is problematic and may lead to unexpected results. */
-//#define CONFIG_BCM_GPIO_EVENTS
+#ifndef CONFIG_BCM_GPIO_EVENTS
+//# define CONFIG_BCM_GPIO_EVENTS
+#endif
 
 /* 1-wire write with pullup support; requires "wire" kernel module patch. */
-//#define CONFIG_WRITE_PULLUP
+#ifndef CONFIG_WRITE_PULLUP
+//# define CONFIG_WRITE_PULLUP
+#endif
 
 /* Fast CRC calculation (using lookup table). */
-#define CONFIG_FAST_CRC
+#ifndef CONFIG_FAST_CRC
+# define CONFIG_FAST_CRC
+#endif
 
 #endif /* __CONFIG_H__ */
